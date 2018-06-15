5b6541e32fe6b208d1d699585c80793e8a204879
Jason Ferguson <fergusonj@ACHRCWK0900L003.usarec.army.mil>
2018-06-15 15:18:10 -0400

    removed some async keywords that weren't needed

--- DBUtils.js
+++ DBUtils.js
@@ -11,11 +11,11 @@ close = (db) => {
     db.close();
 }
 
-query = async (db, sql, params) => {
+query = (db, sql, params) => {
 
     // figured this out from https://stackoverflow.com/questions/47345000/react-native-handling-async-calls-to-sqllite-db
     return new Promise((resolve, reject) => {
-        db.transaction(async (tx) => {
+        db.transaction((tx) => {
 
             tx.executeSql(sql, params, (tx, rs) => {
                 let length = rs.rows.length;
