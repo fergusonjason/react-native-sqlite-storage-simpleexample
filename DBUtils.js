4fbd285bd0884bab9cf6a578e519d58a9b32ccb6
Jason Ferguson <fergusonj@ACHRCWK0900L003.usarec.army.mil>
2018-06-15 15:13:25 -0400

    Finally have a working version once I realized I had to wrap tx.executeSql in a promise

---
+++ DBUtils.js
@@ -0,0 +1,37 @@
+import SQLite from "react-native-sqlite-storage";
+
+open = (dbparams) => {
+
+    let result = SQLite.openDatabase({ name: dbparams.name, createFromLocation: dbparams.createFromLocation });
+
+    return result;
+}
+
+close = (db) => {
+    db.close();
+}
+
+query = async (db, sql, params) => {
+
+    // figured this out from https://stackoverflow.com/questions/47345000/react-native-handling-async-calls-to-sqllite-db
+    return new Promise((resolve, reject) => {
+        db.transaction(async (tx) => {
+
+            tx.executeSql(sql, params, (tx, rs) => {
+                let length = rs.rows.length;
+                let result = [];
+
+                for (i = 0; i < length; i++) {
+                    result.push(rs.rows.item(i));
+                }
+
+                resolve({result});
+            }, (err) => { });
+
+
+        });
+    });
+
+}
+
+export { open, close, query };
