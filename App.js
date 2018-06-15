71a6815dc9544c5fe211c3bea58543b44f1d300f
Jason Ferguson <fergusonj@ACHRCWK0900L003.usarec.army.mil>
2018-06-15 15:13:55 -0400

    Use the DBUtils instead of lots of ugly DB code defined in-place

--- App.js
+++ App.js
@@ -12,47 +12,30 @@ import {
 
 import SQLite from "react-native-sqlite-storage";
 
+import {open,close,query} from "./DBUtils";
+
+SQLite.DEBUG(true);
+SQLite.enablePromise(false);
+
 export default class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = { count: 0 };
 
-
     }
 
-    // componentWillMount does not have access to setState
-    componentDidMount() {
+    // componentWillMount does not have access to setState, so use componentDidMount
+    async componentDidMount() {
 
-        // the "name" amd "createFromLocation" should not be the same. The "real" 
-        // file in /src/main/assets should be specified in createFromLocation
-        db = SQLite.openDatabase({ name: "test.db", createFromLocation: "testdb.db" },
-            () => { console.log("DB Opened") },
-            (err) => { console.log("DB Error: " + err.message) }
-        );
+        let db = await open({name: "test.db", createFromLocation: "testdb.db"});
+        let results = await query(db, "SELECT * from TEST",[]);
 
-        console.log("Db: " + JSON.stringify(db));
+        this.setState({count: results.result.length});
 
-        db.transaction((tx) => {
-            tx.executeSql("select count(*) as count from TEST", [], (tx, results) => {
-
-                var length = results.rows.length;
-                console.log("Result: " + JSON.stringify(results.rows.item(0)));
-                for (i = 0; i < length; i++) {
-                    // by sticking this in the state, you don't have to screw around
-                    // resolving promises
-                    this.setState({ count: results.rows.item(i).count });
-                }
-            });
-        },
-            (err) => {
-                console.warn("DB Error: " + err.message)
-            });
     }
 
     componentWillUnmount() {
-
-        db.close();
 
     }
 
